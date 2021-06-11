f3ac986cc6da5abdff0d813e763f4d953422e532
Naman Subedi <naman.subedi12@gmail.com>
2021-06-11 12:42:12 +0545

    added fixes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# project for comp202
-# run with python 3.x
+# Project for COMP202
+Run with Python 3.x
+
+The iterative method can be found on the iterative.py file.
+The recursive method can be found on the recursive.py file.
+
+List.txt serves as a sorted list that the program takes into an array to perform the binary search operations on.
+
+For the partial fulfillment of 3rd Sem COMP202 course.
--- recursive.py
+++ recursive.py
@@ -1,30 +1,21 @@
-
+from time import perf_counter_ns
 
 def binary_search(arr, low, high, x):
-
-	# Check base case
+	
 	if high >= low:
-
 		mid = (high + low) // 2
-
-		# If element is present at the middle itself
 		if arr[mid] == x:
 			return mid
 
-		# If element is smaller than mid, then it can only
-		# be present in left subarray
 		elif arr[mid] > x:
 			return binary_search(arr, low, mid - 1, x)
 
-		# Else the element can only be present in right subarray
 		else:
 			return binary_search(arr, mid + 1, high, x)
 
 	else:
-		# Element is not present in the array
+		
 		return -1
-
-
 
 arr = []
 
@@ -43,15 +34,20 @@ x = x + "\n"
 
 
 # Function call
+
+t1_start = perf_counter_ns()
 result = binary_search(arr, 0, len(arr)-1, x)
+t1_stop = perf_counter_ns()
 
 
 
 
 if result != -1:   
-   print("Element is present at index", str(result))  
+   print("Element is present at index", str(result+1))  
+   
    print("______________________")
 else:   
    print("Element is not present in array")  
    
-
+t = t1_stop-t1_start
+print("The runtime performance: " + str(t) + " ns.")
