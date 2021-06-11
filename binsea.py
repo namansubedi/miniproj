51b6234f90940057eb3f3f61367ab07b4a58c9d9
Ishwarkc133 <ishwarkc133@gmail.com>
2021-06-11 12:13:55 +0545

    recursive

--- binsea.py
+++ binsea.py
@@ -1,6 +1,6 @@
 from time import perf_counter_ns
 
-#performance compare function
+#performance compare function"
 def perf(arr):
     target = ["a", "matter", "yourself"]
     for tar in target:
@@ -31,27 +31,29 @@ def find(L, target):
 arr = []
 
 file = open('list.txt', 'r')
-for each in file:
-    each = each.lower()
-    arr.append(each)
+i=0;
+for i in range(5):
+    for each in file:
+        each = each.lower()
+        arr.append(each)
 
-#take search term
-print("What value do you wanna find in the array?")
-x = str(input("Enter your value: "))
-x = x.lower()
-x = x + "\n"
+    #take search term
+    print("What value do you wanna find in the array?")
+    x = str(input("Enter your value: "))
+    x = x.lower()
+    x = x + "\n"
 
-#run this to check and compare performance comment this to run program normally
-#perf(arr)
+    #run this to check and compare performance comment this to run program normally
+    perf(arr)
 
-t1_start = perf_counter_ns()
-result = find(arr, x)
-t1_stop = perf_counter_ns()
+    t1_start = perf_counter_ns()
+    result = find(arr, x)
+    t1_stop = perf_counter_ns()
 
-if (result is None):
-	print("Element not present in the list. Rerun program.")
-else:
-	print("Element found at line: " , result+1)
+    if (result is None):
+        print("Element not present in the list. Rerun program.")
+    else:
+        print("Element found at line: " , result+1)
 
-t = t1_stop-t1_start
-print("The runtime performance: " + str(t) + " ns. Jumps = " + str(jump))
+    t = t1_stop-t1_start
+    print("The runtime performance: " + str(t) + " ns. Jumps = " + str(jump))
